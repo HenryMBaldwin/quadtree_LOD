f68087885d526593924881349b3da30eb855230c
Henry Baldwin <henrymbaldwin@gmail.com>
2024-08-26 15:41:11 -0400

    Hello world with macroquad

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+#[macroquad::main("quadmap_LOD")]
+async fn main() {
+    loop {
+        clear_background(BLACK);
+        draw_text("Hello, world!", 20.0, 20.0, 30.0, WHITE);
+        next_frame().await
+    }
 }
